4076502318a9cbc796783a6c431621097692baa0
minyee2913 <72301973+minyee2913@users.noreply.github.com>
2020-10-05 10:36:35 +0900

    Add files via upload

---
+++ scorepay(EN_US).js
@@ -0,0 +1,33 @@
+﻿import {
+	executeCommand,
+	registerCommand,
+	registerOverride
+} from "ez:command";
+
+//edit the percentage of tax here
+let taxPercentage = 0
+//edit the least amount of money a player can send
+let leastAmount = 100
+//edit the scoreboard object of money
+const score = "money"
+
+registerCommand("pay", "Transfer balance to other players.", 0);
+registerOverride("pay", [{type: "players", name: "player", optional: false}, {type: "int", name: "amount", optional: false}], function (targets, count) {
+	if (targets.length != 1) throw "You can only pay money to 1 player at a time.";
+	let sender = this.player
+	let target = targets[0];
+	let senderName = sender.name
+	let targetName = target.name
+	let finalAmount = Math.round((100 - taxPercentage) * count / 100)
+	if (this.player) {
+		if (count < leastAmount) {
+			throw "최소 $" + leastAmount + " 원 이상의 금액만 보낼 수 있습니다";
+		}else {
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ scoreboard players remove @s[scores={" + score + "=" + count + "..}] " + score + " " + count + "");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ scoreboard players add \"" + targetName + "\" " + score + " " + count + "");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ tellraw @s {\"rawtext\":[{\"text\":\"§l§6You send §b" + targetName + " §e" + count + " \n§atax : " + taxPercentage + "% §6They get §e" + finalAmount + "\"}]}");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ tellraw \"" + targetName + "\" {\"rawtext\":[{\"text\":\"§l§b" + senderName + " §6send you §e" + count + "\n§atax : " + taxPercentage + "% §6you got §e" + finalAmount + "\"}]}");
+		}
+	}
+});
+console.log("2913scorepay(en).js loaded");
---
+++ scorepay(KO_KR).js
@@ -0,0 +1,52 @@
+﻿import {
+	executeCommand,
+	registerCommand,
+	registerOverride
+} from "ez:command";
+
+//세금
+let taxPercentage = 0
+//돈 보낼 수 있는 최소 금액
+let leastAmount = 100
+//돈으로 인식할 스코어보드
+const score = "money"
+
+registerCommand("pay", "다른 플레이어에게 돈을 보냅니다", 0);
+registerOverride("pay", [{type: "players", name: "player", optional: false}, {type: "int", name: "amount", optional: false}], function (targets, count) {
+	if (targets.length != 1) throw "You can only pay money to 1 player at a time.";
+	let sender = this.player
+	let target = targets[0];
+	let senderName = sender.name
+	let targetName = target.name
+	let finalAmount = Math.round((100 - taxPercentage) * count / 100)
+	if (this.player) {
+		if (count < leastAmount) {
+			throw "최소 $" + leastAmount + " 원 이상의 금액만 보낼 수 있습니다";
+		}else {
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ scoreboard players remove @s[scores={" + score + "=" + count + "..}] " + score + " " + count + "");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ scoreboard players add \"" + targetName + "\" " + score + " " + count + "");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ tellraw @s {\"rawtext\":[{\"text\":\"§l§b" + targetName + "§f 님께 §e" + count + "§f 원을 보냈습니다 \n§a세금 : " + taxPercentage + "% §6보내진 최종 금액 §e" + finalAmount + "\"}]}");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ tellraw \"" + targetName + "\" {\"rawtext\":[{\"text\":\"§l§b" + senderName + " §f님이 §e" + count + "§f 원을 보내주셨습니다\n§a세금 : " + taxPercentage + "% §6최종 금액 §e" + finalAmount + "\"}]}");
+		}
+	}
+});
+registerCommand("돈주기", "다른 플레이어에게 돈을 보냅니다", 0);
+registerOverride("돈주기", [{type: "players", name: "player", optional: false}, {type: "int", name: "amount", optional: false}], function (targets, count) {
+	if (targets.length != 1) throw "You can only pay money to 1 player at a time.";
+	let sender = this.player
+	let target = targets[0];
+	let senderName = sender.name
+	let targetName = target.name
+	let finalAmount = Math.round((100 - taxPercentage) * count / 100)
+	if (this.player) {
+		if (count < leastAmount) {
+			throw "최소 $" + leastAmount + " 원 이상의 금액만 보낼 수 있습니다";
+		}else {
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ scoreboard players remove @s[scores={" + score + "=" + count + "..}] Exception ReferenceError: " + score + " " + count + "");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ scoreboard players add \"" + targetName + "\" " + score + " " + count + "");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ tellraw @s {\"rawtext\":[{\"text\":\"§l§b" + targetName + "§f 님께 §e" + count + "§f 원을 보냈습니다 \n§a세금 : " + taxPercentage + "% §6보내진 최종 금액 §e" + finalAmount + "\"}]}");
+			executeCommand("execute \"" + senderName + "\" ~ ~ ~ execute @s[scores={" + score + "=" + count + "..}] ~ ~ ~ tellraw \"" + targetName + "\" {\"rawtext\":[{\"text\":\"§l§b" + senderName + " §f님이 §e" + count + "§f 원을 보내주셨습니다\n§a세금 : " + taxPercentage + "% §6최종 금액 §e" + finalAmount + "\"}]}");
+		}
+	}
+});
+console.log("2913scorepay(ko).js loaded");
